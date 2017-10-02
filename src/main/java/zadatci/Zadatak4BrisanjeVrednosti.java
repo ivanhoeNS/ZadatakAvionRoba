a0b41990357bc0acf13bd8b26a99af393fb87277
Ivan <ivan.wer@gmail.com>
2017-10-02 19:14:55 +0200

    Kreirane klase za zadatke

---
+++ src/main/java/zadatci/Zadatak4BrisanjeVrednosti.java
@@ -0,0 +1,33 @@
+package zadatci;
+
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Zadatak4BrisanjeVrednosti {
+
+    public static void main(String[] args){
+        ConnectionSource connectionSource = null;
+        try {
+            // create our data-source for the database
+            connectionSource = new JdbcConnectionSource(
+                    "jdbc:sqlite:avionRoba.db");
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if (connectionSource != null) {
+                try {
+                    connectionSource.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
